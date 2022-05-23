47e85463cb219b201edcd381b21f6ce99c6c29fe
D Ther <dtherhtun.cw@gmail.com>
2022-05-23 20:31:51 +0700

    add new HR plus-minus

---
+++ HackerRank/plus-minus/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func plusMinus(arr []int32) {
+	// Write your code here
+	var zero int
+	var plus int
+	var minus int
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == 0 {
+			zero += 1
+		} else if arr[i] < 0 {
+			minus += 1
+		} else {
+			plus += 1
+		}
+	}
+	fmt.Printf("%.6f\n", float64(plus)/float64(len(arr)))
+	fmt.Printf("%.6f\n", float64(minus)/float64(len(arr)))
+	fmt.Printf("%.6f\n", float64(zero)/float64(len(arr)))
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	plusMinus(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
