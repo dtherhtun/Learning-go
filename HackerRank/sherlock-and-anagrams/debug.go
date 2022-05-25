0f9ea29365c7b481c34242d9f5e76b058c7690a5
D Ther <dtherhtun.cw@gmail.com>
2022-05-26 01:03:04 +0700

    add sherlock-and-anagrams

---
+++ HackerRank/sherlock-and-anagrams/debug.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func main() {
+	s := "abba"
+	var result int
+	for i := 1; i < len(s); i++ {
+		fmt.Println("outer-i=", i)
+		m := make(map[string]int)
+		fmt.Println("m=", m)
+		for j := 0; j < len(s)-i+1; j++ {
+			fmt.Println("j=", j)
+			fmt.Println("s=", s)
+			sa := strings.Split(s, "")
+			fmt.Println("split=", sa)
+			b := sa[j : j+i]
+			fmt.Println("before-sort=", b, "j:j+i=", j, ":", j+i)
+			sort.Strings(b)
+			fmt.Println("after-sort=", b)
+			sub := strings.Join(b, "")
+			fmt.Println("substring=", sub)
+		
+			if _, ok := m[sub]; ok {
+				m[sub] += 1
+				fmt.Println("m-exist=", m[sub])
+			} else {
+				fmt.Println("m-not-exist=", m[sub])
+				m[sub] = 1
+			}
+			result += m[sub] - 1
+			fmt.Println("m-inner=", m, " m-sub=", m[sub])
+			fmt.Println("inner-result=", result)
+
+		}
+
+	}
+	fmt.Println("result=", result)
+}
+
---
+++ HackerRank/sherlock-and-anagrams/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+    "sort"
+)
+
+/*
+ * Complete the 'sherlockAndAnagrams' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+func sherlockAndAnagrams(s string) int32 {
+    // Write your code here
+    var result int32 
+    for i := 1; i < len(s); i++ {
+        m := make(map[string]int32)
+        for j := 0; j < len(s)-i+1; j++ {
+            s := strings.Split(s, "") 
+            sa := s[j:j+i]   
+            sort.Strings(sa)
+            sub := strings.Join(sa, "")
+            
+            if _, ok := m[sub]; ok {
+                m[sub] += 1
+            } else {
+                m[sub] = 1
+            }
+            result+=m[sub]-1
+        }
+    }
+    return result
+
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    qTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    q := int32(qTemp)
+
+    for qItr := 0; qItr < int(q); qItr++ {
+        s := readLine(reader)
+
+        result := sherlockAndAnagrams(s)
+
+        fmt.Fprintf(writer, "%d\n", result)
+    }
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
