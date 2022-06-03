070b42687a3a4b19712bc3fec09802abcb307b08
D Ther <dtherhtun.cw@gmail.com>
2022-06-03 19:02:59 +0700

    day-of-the-programmer

---
+++ HackerRank/day-of-the-programmer/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+    "time"
+)
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+func dayOfProgrammer(year int32) string {
+    // Write your code here
+    date := time.Date(int(year), time.Month(3), 0, 0, 0, 0, 0, time.UTC)
+    feb := date.Day()
+    d_o_p := 256 - (31 + feb + 31 + 30 + 31 + 30 + 31 + 31)
+    if year > 1918 {
+    return fmt.Sprintf("%d.09.%d",d_o_p,date.Year()) 
+    } else if year < 1918 {
+        if year%4 == 0 {
+            return fmt.Sprintf("12.09.%d",year)
+        } else {
+            return fmt.Sprintf("13.09.%d",year)
+        }
+    } else {
+        return fmt.Sprintf("26.09.%d",year)
+    }
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    yearTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    year := int32(yearTemp)
+
+    result := dayOfProgrammer(year)
+
+    fmt.Fprintf(writer, "%s\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
---
+++ HackerRank/migratory-birds/main.go
@@ -0,0 +1,84 @@
+// https://www.hackerrank.com/challenges/migratory-birds/problem
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func migratoryBirds(arr []int32) int32 {
+    // Write your code here
+    var count int32
+    var max int
+    m := make(map[int32]int)
+    for _, v := range arr {
+        m[v] = m[v]+1
+    }
+    for k,v := range m {
+        if v > max {
+            max = v
+        }
+        if m[k] == max {
+            count = k
+        }
+    }
+    return count
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    arrCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var arr []int32
+
+    for i := 0; i < int(arrCount); i++ {
+        arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+        checkError(err)
+        arrItem := int32(arrItemTemp)
+        arr = append(arr, arrItem)
+    }
+
+    result := migratoryBirds(arr)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
