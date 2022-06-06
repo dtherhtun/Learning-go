30c02cb68e8d0c3266a082cf6764024fbe649a0a
D Ther <dtherhtun.cw@gmail.com>
2022-06-06 21:50:10 +0700

    counting-valleys

---
+++ HackerRank/counting-valleys/main.go
@@ -0,0 +1,79 @@
+// hackerrank.com/challenges/counting-valleys/problem
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+func countingValleys(steps int32, path string) int32 {
+    // Write your code here
+    var current_level int32
+    var valleys int32
+    paths := strings.Split(path, "")
+
+    for i := 0; i < int(steps); i++ {
+        if paths[i] == "U" {
+            current_level++
+            if current_level == 0 {
+                valleys++
+            }
+        } else if paths[i] == "D" {
+            current_level--
+        }
+    }
+    return valleys
+
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    stepsTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    steps := int32(stepsTemp)
+
+    path := readLine(reader)
+
+    result := countingValleys(steps, path)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
