4d0adf724de7e0baed3eb6a6211e8d8eeb07172f
D Ther <dtherhtun.cw@gmail.com>
2022-06-11 22:11:31 +0700

    utopian-tree

---
+++ HackerRank/utopian-tree/main.go
@@ -0,0 +1,75 @@
+// hackerrank.com/challenges/utopian-tree/problem
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'utopianTree' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+func utopianTree(n int32) int32 {
+    // Write your code here
+   var high int32
+   for i := int32(0); i <= n; i++ {
+       if i%2 == 0 {
+           high += 1
+       } else {
+           high = high + high
+       }
+   }
+   return high
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    tTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    t := int32(tTemp)
+
+    for tItr := 0; tItr < int(t); tItr++ {
+        nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+        checkError(err)
+        n := int32(nTemp)
+
+        result := utopianTree(n)
+
+        fmt.Fprintf(writer, "%d\n", result)
+    }
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
