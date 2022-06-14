34156f4c300e3d302476f7238257cff5c0c71d86
D Ther <dtherhtun.cw@gmail.com>
2022-06-14 23:16:33 +0700

    a tale of two cities

---
+++ GBT/015_a_tale_of_two_cities/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	city1, city2 := "Kraków", "Kraków"
+	fmt.Println(city1 == city2)
+}
---
+++ GBT/015_a_tale_of_two_cities/two_cities.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	city1, city2 := "Kraków", "Kraków"
+	fmt.Println(city1 == city2)
+}
