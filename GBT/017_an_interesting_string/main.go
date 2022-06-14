6a603853476ac3013ef998d9aeb95aec115778bb
D Ther <dtherhtun.cw@gmail.com>
2022-06-14 23:35:25 +0700

    an interesting string

---
+++ GBT/017_an_interesting_string/main.go
@@ -0,0 +1,14 @@
+// The string type supports type conversion from int, 
+// it’ll treat this integer as a rune. 
+// The rune 169 is the copyright sign (©)
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 169
+	s := string(i)
+	fmt.Println(s)
+}
---
+++ GBT/017_an_interesting_string/strint_map.go
@@ -0,0 +1,17 @@
+// When you convert from byte slice to string, 
+// Go will copy the byte slice - which does a memory allocation. 
+// In maps, where you can’t use a []byte as a key, there is a compiler optimization:
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := map[string]int{
+		"hello": 3,
+	}
+	key := []byte{'h', 'e', 'l', 'l', 'o'}
+	val := m[string(key)] // no memory allocation
+	fmt.Println(val)
+}
