55366e3bbab629771fbf102cdf3a894f1f268ddb
D Ther <dtherhtun.cw@gmail.com>
2022-06-14 14:03:54 +0700

    permutation equation

---
+++ HackerRank/permutation-equation/main.go
@@ -0,0 +1,96 @@
+// https://www.hackerrank.com/challenges/permutation-equation/problem
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'permutationEquation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY p as parameter.
+ */
+
+func permutationEquation(p []int32) []int32 {
+    // Write your code here
+    var history []int32
+    var position []int
+    
+    for j := int32(1); j <= int32(len(p)); j++ {
+        for k, v := range p {
+            if j == v {
+            position = append(position, k+1)
+            }
+        }
+    }
+    for i := 0; i < len(position); i++ {
+        for k, v := range p {
+            if int32(position[i]) == v {
+                history = append(history, int32(k+1))
+            }
+        }
+    }
+    return history
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    pTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var p []int32
+
+    for i := 0; i < int(n); i++ {
+        pItemTemp, err := strconv.ParseInt(pTemp[i], 10, 64)
+        checkError(err)
+        pItem := int32(pItemTemp)
+        p = append(p, pItem)
+    }
+
+    result := permutationEquation(p)
+
+    for i, resultItem := range result {
+        fmt.Fprintf(writer, "%d", resultItem)
+
+        if i != len(result) - 1 {
+            fmt.Fprintf(writer, "\n")
+        }
+    }
+
+    fmt.Fprintf(writer, "\n")
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
