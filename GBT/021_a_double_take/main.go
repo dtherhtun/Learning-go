cd5a2daefaf412770662534e09bea20546748f84
D Ther <dtherhtun.cw@gmail.com>
2022-06-15 20:17:31 +0700

    a double take

---
+++ GBT/021_a_double_take/main.go
@@ -0,0 +1,20 @@
+// init is the final stage in package initialization after imported 
+// modules are initialized and package-level variables are initialized.
+
+package main
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Printf("A ")
+}
+
+func init() {
+	fmt.Print("B ")
+}
+
+func main() {
+	fmt.Println()
+}
