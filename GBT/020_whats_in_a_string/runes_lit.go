b59df1b6e217201ee114fd79832b2431b6c1cc4c
D Ther <dtherhtun.cw@gmail.com>
2022-06-15 20:06:35 +0700

    runes lit

---
+++ GBT/020_whats_in_a_string/runes_lit.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	r1 := '™'
+	fmt.Println(r1)
+	fmt.Printf("%c\n", r1)
+
+	r2 := '\x61'
+	fmt.Printf("%c\n", r2)
+
+	r3 := '\u2122'
+	fmt.Printf("%c\n", r3)
+
+	r4 := '\U00002122'
+	fmt.Printf("%c\n", r4)
+}
