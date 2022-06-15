6dfcbb886c252370f7e8444ec0f1aeefa71997cc
D Ther <dtherhtun.cw@gmail.com>
2022-06-15 19:58:43 +0700

    waht's in a string

---
+++ GBT/020_whats_in_a_string/main.go
@@ -0,0 +1,19 @@
+// Go strings are UTF-8 encoded. When you access a string with [] or with len, 
+// Go will access the underlying []byte. byte is a type in Go that’s aliased to uint8.
+// When you iterate over a string in Go, you will get the Unicode character - 
+// called rune which is an alias to int32. This is due to the fact the characters in UTF-8 can be up to 4 bytes.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	msg := "π = 3.14159265358..."
+	fmt.Printf("%T ", msg[0])
+	for _, c := range msg {
+		fmt.Printf("%T\n", c)
+		break
+	}
+}
