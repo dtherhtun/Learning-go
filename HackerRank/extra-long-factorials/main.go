841517dd8d7e45b652fd514f472563c90325112b
D Ther <dtherhtun.cw@gmail.com>
2022-06-17 00:10:55 +0700

    extra long factorials

---
+++ HackerRank/extra-long-factorials/main.go
@@ -0,0 +1,57 @@
+// https://www.hackerrank.com/challenges/extra-long-factorials/problem
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+    "math/big"
+)
+
+/*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+func extraLongFactorials(n int32) {
+    // Write your code here
+    r := big.NewInt(1)
+
+    for i := int32(0); i < n; i++ {
+
+        r.Mul(r, big.NewInt(int64(n-i)))
+    }
+
+    fmt.Println(r)
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    extraLongFactorials(n)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
