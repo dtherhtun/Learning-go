fc52b9821e0485019fd42a46c766337af1edbe05
D Ther <dtherhtun.cw@gmail.com>
2022-06-16 22:33:32 +0700

    find digits

---
+++ HackerRank/find-digits/main.go
@@ -0,0 +1,84 @@
+// https://www.hackerrank.com/challenges/find-digits/problem
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+func findDigits(n int32) int32 {
+    // Write your code here
+    var arr []int32
+    var result int32
+    var mod int32
+    num := n
+
+    for n > 0 {
+        mod = n % 10
+        n = n / 10
+        if mod != 0 {
+            arr = append(arr, mod)
+        }
+    }
+    for _, v := range arr {
+        if num % v == 0 {
+            result++
+        }
+    }
+    return result
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    tTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    t := int32(tTemp)
+
+    for tItr := 0; tItr < int(t); tItr++ {
+        nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+        checkError(err)
+        n := int32(nTemp)
+
+        result := findDigits(n)
+
+        fmt.Fprintf(writer, "%d\n", result)
+    }
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
