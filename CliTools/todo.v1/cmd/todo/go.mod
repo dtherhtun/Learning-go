463232adcba07c51dce6440845195d6e61129c37
D Ther <dtherhtun.cw@gmail.com>
2022-07-04 19:20:38 +0700

    add todo

---
+++ CliTools/todo.v1/cmd/todo/go.mod
@@ -0,0 +1,3 @@
+module github.com/dtherhtun/Learning-go/CliTools/todo.v1/cmd/todo
+
+go 1.17
---
+++ CliTools/todo.v1/cmd/todo/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/dtherhtun/Learning-go/CliTools/todo"
+)
+
+const todoFileName = ".todo.json"
+
+func main() {
+	task := flag.String("task", "", "Task to be include in todo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	flag.Parse()
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case *list:
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *task != "":
+		l.Add(*task)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
+	}
+}
