0ece79228c074b17b6ef9becf10a286afa7d9c19
D Ther <dtherhtun.cw@gmail.com>
2022-07-12 23:52:48 +0700

    add mdp.v1

--- CliTools/workingFiles/mdp.v1/go.mod
+++ CliTools/workingFiles/mdp.v1/go.mod
@@ -1,4 +1,4 @@
-module github.com/dtherhtun/Learning-go/CliTools/workingFiles/mdp
+module github.com/dtherhtun/Learning-go/CliTools/workingFiles/mdp.v1
 
 go 1.17
 
---
+++ CliTools/workingFiles/mdp/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+const (
+	inputFile  = "./testdata/test1.md"
+	resultFile = "test1.md.html"
+	goldenFile = "./testdata/test1.md.html"
+)
+
+func TestParseContent(t *testing.T) {
+	input, err := ioutil.ReadFile(inputFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	result := paraseContent(input)
+
+	expeced, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(expeced, result) {
+		t.Logf("golden:\n%s\n", expeced)
+		t.Logf("result:\n%s\n", result)
+		t.Error("Result content does not match golden file")
+	}
+}
+
+func TestRun(t *testing.T) {
+	if err := run(inputFile); err != nil {
+		t.Fatal(err)
+	}
+
+	result, err := ioutil.ReadFile(resultFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected, err := ioutil.ReadFile(goldenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(expected, result) {
+		t.Logf("golden:\n%s\n", expected)
+		t.Logf("result:\n%s\n", result)
+		t.Error("Result content does not match golden file")
+	}
+
+	os.Remove(resultFile)
+}
