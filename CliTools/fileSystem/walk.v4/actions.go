b5822d6bc14eb1adf6176c6c3820890aabd2d6e9
D Ther <dtherhtun.cw@gmail.com>
2022-07-30 09:51:55 +0700

    exercise

---
+++ CliTools/fileSystem/walk.v4/actions.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func filterOut(path, ext string, minSize int64, info os.FileInfo) bool {
+	if info.IsDir() || info.Size() < minSize {
+		return true
+	}
+
+	if ext != "" && filepath.Ext(path) != ext {
+		return true
+	}
+	return false
+}
+
+func listFile(path string, out io.Writer) error {
+	_, err := fmt.Fprintln(out, path)
+	return err
+}
+
+func delFile(path string, delLogger *log.Logger) error {
+	if err := os.Remove(path); err != nil {
+		return err
+	}
+	delLogger.Println(path)
+	return nil
+}
+
+func archiveFile(distDir, root, path string) error {
+	info, err := os.Stat(distDir)
+	if err != nil {
+		return err
+	}
+
+	if !info.IsDir() {
+		return fmt.Errorf("%s is not a directory", distDir)
+	}
+
+	relDir, err := filepath.Rel(root, filepath.Dir(path))
+	if err != nil {
+		return err
+	}
+
+	dest := fmt.Sprintf("%s.gz", filepath.Base(path))
+	targetPath := filepath.Join(distDir, relDir, dest)
+	if err := os.MkdirAll(filepath.Dir(targetPath), 0755); err != nil {
+		return err
+	}
+
+	out, err := os.OpenFile(targetPath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	in, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	zw := gzip.NewWriter(out)
+	zw.Name = filepath.Base(path)
+
+	if _, err := io.Copy(zw, in); err != nil {
+		return err
+	}
+
+	if err := zw.Close(); err != nil {
+		return err
+	}
+
+	return out.Close()
+}
