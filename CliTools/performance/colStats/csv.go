3131501ea59d7fa24a43f796422dd0e0420c25b1
D Ther <dtherhtun.cw@gmail.com>
2022-08-13 13:49:24 +0700

    colStats

--- CliTools/performance/colStats/csv.go
+++ CliTools/performance/colStats/csv.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+)
+
 func sum(data []float64) float64 {
 	sum := 0.0
 	for _, v := range data {
@@ -10,4 +17,33 @@ func sum(data []float64) float64 {
 
 func avg(data []float64) float64 {
 	return sum(data) / float64(len(data))
+}
+
+type statsFunc func(data []float64) float64
+
+func csv2float(r io.Reader, column int) ([]float64, error) {
+	cr := csv.NewReader(r)
+	column--
+	allData, err := cr.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("cannot read data from file: %w", err)
+	}
+
+	var data []float64
+
+	for i, row := range allData {
+		if i == 0 {
+			continue
+		}
+
+		if len(row) <= column {
+			return nil, fmt.Errorf("%w: File has only %d cloumns", ErrInvaildColumn, len(row))
+		}
+		v, err := strconv.ParseFloat(row[column], 64)
+		if err != nil {
+			return nil, fmt.Errorf("%w: %s", ErrNotNumber, err)
+		}
+		data = append(data, v)
+	}
+	return data, nil
 }
