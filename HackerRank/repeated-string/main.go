6f8ac67b260ebf718ef600d47421a548c27d7614
D Ther <dtherhtun.cw@gmail.com>
2022-08-21 17:41:29 +0700

    repeated-string

---
+++ HackerRank/repeated-string/main.go
@@ -0,0 +1,78 @@
+// https://www.hackerrank.com/challenges/repeated-string/problem
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+func repeatedString(s string, n int64) int64 {
+    // Write your code here
+    var answer int64
+
+    aCount := int64(strings.Count(s, "a"))
+
+    repeats := n / int64(len(s))
+    remainder := n % int64(len(s))
+
+    if remainder > 0 {
+        aCountInRemainder := strings.Count(s[:remainder], "a")
+        answer = int64((aCount * repeats) + int64(aCountInRemainder))
+    } else {
+        answer = int64((aCount * repeats))
+    }
+
+    return answer
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    s := readLine(reader)
+
+    n, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    result := repeatedString(s, n)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
