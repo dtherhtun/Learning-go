9e5654652fd97e071158f5857fafb90eaef2db96
D Ther <dtherhtun.cw@gmail.com>
2022-08-22 21:30:38 +0700

    jumping-on-the-clouds

---
+++ HackerRank/jumping-on-the-clouds/main.go
@@ -0,0 +1,85 @@
+// https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'jumpingOnClouds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY c as parameter.
+ */
+
+func jumpingOnClouds(c []int32) int32 {
+	// Write your code here
+	var jumpcount, i int32
+
+	for i < int32(len(c)-1) {
+
+		if i+2 < int32(len(c)) && c[i+2] == 0 {
+			jumpcount++
+			i += 2
+			continue
+		}
+		if i+1 < int32(len(c)) && c[i+1] == 0 {
+			jumpcount++
+			i += 1
+		}
+	}
+	return jumpcount
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	cTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var c []int32
+
+	for i := 0; i < int(n); i++ {
+		cItemTemp, err := strconv.ParseInt(cTemp[i], 10, 64)
+		checkError(err)
+		cItem := int32(cItemTemp)
+		c = append(c, cItem)
+	}
+
+	result := jumpingOnClouds(c)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
