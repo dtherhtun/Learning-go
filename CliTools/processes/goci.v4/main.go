a6aa2d90e9065a8209637aa79288973511b261a7
D Ther <dtherhtun.cw@gmail.com>
2022-09-16 00:42:10 +0700

    goci v4

---
+++ CliTools/processes/goci.v4/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+type executer interface {
+	execute() (string, error)
+}
+
+func main() {
+	proj := flag.String("p", "", "Project directory")
+	flag.Parse()
+
+	if err := run(*proj, os.Stdout); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(proj string, out io.Writer) error {
+	if proj == "" {
+		return fmt.Errorf("project directory is required: %w", ErrorValidation)
+	}
+	pipeline := make([]executer, 3)
+	pipeline[0] = newStep(
+		"go build",
+		"go",
+		"Go Build: SUCCESS",
+		proj,
+		[]string{"build", ".", "errors"},
+	)
+	pipeline[1] = newStep(
+		"go test",
+		"go",
+		"Go Test: SUCCESS",
+		proj,
+		[]string{"test", "-v"},
+	)
+	pipeline[2] = newExceptionStep(
+		"go fmt",
+		"gofmt",
+		"Gofmt: SUCCESS",
+		proj,
+		[]string{"-l", "."},
+	)
+	for _, s := range pipeline {
+		msg, err := s.execute()
+		if err != nil {
+			return err
+		}
+		_, err = fmt.Fprintln(out, msg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
