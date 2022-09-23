65e5e568a234dc96a8e4b8e74c0d8885d391e564
D Ther <dtherhtun.cw@gmail.com>
2022-09-24 01:54:36 +0700

    Yay!

--- k8s/deploy02/go.mod
+++ k8s/deploy02/go.mod
@@ -3,8 +3,10 @@ module github.com/dtherhtun/Learning-go/k8s/deploy01
 go 1.19
 
 require (
-	k8s.io/api v0.25.0
-	k8s.io/client-go v0.25.0
+	k8s.io/api v0.25.2
+	k8s.io/apimachinery v0.25.2
+	k8s.io/cli-runtime v0.25.2
+	k8s.io/client-go v0.25.2
 )
 
 require (
@@ -12,13 +14,13 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/apimachinery v0.25.0 // indirect
 	k8s.io/klog/v2 v2.70.1 // indirect
 	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
 	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
--- k8s/deploy02/main.go
+++ k8s/deploy02/main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/cli-runtime/pkg/printers"
+	"k8s.io/client-go/kubernetes/scheme"
 	"log"
 	"os"
 	"strings"
-
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/client-go/kubernetes/scheme"
 )
 
 func main() {
-	updateCPU := 2
-	fname := "deployment.yaml"
+	//updateCPU := 2
+	fname := "nginx-deployment.yaml"
 	data, err := os.ReadFile(fname)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(ReadCPU(data))
+	UpdateCPU(data, fname)
+
 }
 
 func ReadCPU(data []byte) string {
@@ -47,4 +51,36 @@ func ReadCPU(data []byte) string {
 		}
 	}
 	return CpuValue
+}
+
+func int32Ptr(i int32) *int32 { return &i }
+
+func UpdateCPU(data []byte, fname string) {
+
+	//mydeployment := &appsv1.Deployment{}
+	//err := yaml.Unmarshal(data, &mydeployment)
+	//if err != nil {
+	//	fmt.Println("Yaml Unmarshall", err)
+	//}
+	decoder := scheme.Codecs.UniversalDeserializer()
+	obj, _, err := decoder.Decode(
+		data,
+		nil,
+		&appsv1.Deployment{})
+
+	if err != nil {
+		log.Print(err)
+	}
+	mydeployment := obj.(*appsv1.Deployment)
+
+	mydeployment.Spec.Replicas = int32Ptr(2)
+
+	mydeployment.Spec.Template.Spec.Containers[0].Resources.Requests = make(map[corev1.ResourceName]resource.Quantity)
+	mydeployment.Spec.Template.Spec.Containers[0].Resources.Requests[corev1.ResourceCPU] = *resource.NewMilliQuantity(5300, resource.DecimalSI)
+	mydeployment.Spec.Template.Spec.Containers[0].Resources.Requests[corev1.ResourceMemory] = *resource.NewQuantity(5*1024*1024*1024, resource.BinarySI)
+	newFile, _ := os.Create(fname)
+	//manifest, _ := os.Open(fname)
+	y := printers.YAMLPrinter{}
+	defer newFile.Close()
+	y.PrintObj(obj, newFile)
 }
