8ee9fdedcc7a7e48d66a0eea75b6bb7c7a51bd91
D Ther <dtherhtun.cw@gmail.com>
2022-10-23 00:07:03 +0700

    feat(v3): complete + delete

---
+++ CliTools/apis/todoClient.v3/cmd/del.go
@@ -0,0 +1,78 @@
+/*
+Copyright © 2022 De Thar Htun
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// delCmd represents the del command
+var delCmd = &cobra.Command{
+	Use:          "del <id>",
+	Short:        "Deletes an item from the list",
+	SilenceUsage: true,
+	Args:         cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		apiRoot := viper.GetString("api-root")
+
+		return delAction(os.Stdout, apiRoot, args[0])
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(delCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// delCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// delCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func delAction(out io.Writer, apiRoot, arg string) error {
+	id, err := strconv.Atoi(arg)
+	if err != nil {
+		return fmt.Errorf("%w: Item id must be a number", ErrNotNumber)
+	}
+
+	if err := deleteItem(apiRoot, id); err != nil {
+		return err
+	}
+
+	return printDel(out, id)
+}
+
+func printDel(out io.Writer, id int) error {
+	_, err := fmt.Fprintf(out, "Item number %d deleted.\n", id)
+
+	return err
+}
