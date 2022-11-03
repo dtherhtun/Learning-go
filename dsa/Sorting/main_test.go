6c5281ab9939415bdc6d480be36e4cfd77e3a71f
D Ther <dtherhtun.cw@gmail.com>
2022-11-03 23:28:26 +0700

    feat(dsa): QuickSort

--- dsa/Sorting/main_test.go
+++ dsa/Sorting/main_test.go
@@ -69,3 +69,19 @@ func BenchmarkMergeSort(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkQuickSort(b *testing.B) {
+	inputSize := []int{10, 100, 1000, 10000, 100000}
+	for _, size := range inputSize {
+		b.Run(fmt.Sprintf("input_size_%d", size), func(b *testing.B) {
+			testList := make([]int, size)
+			for i := 0; i < size; i++ {
+				testList[i] = rand.Intn(size)
+			}
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				quickSort(testList, 0, len(testList)-1)
+			}
+		})
+	}
+}
