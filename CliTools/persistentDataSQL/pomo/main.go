3185113ed1e3df70a9e4eee6ea8588c6c63ce089
D Ther <dtherhtun.cw@gmail.com>
2022-12-02 21:49:47 +0700

    Summarize bar and line Chart

--- CliTools/persistentDataSQL/pomo/main.go
+++ CliTools/persistentDataSQL/pomo/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/dtherhtun/Learning-go/CliTools/interactiveTools/pomo/cmd"
 )
 
 func main() {
 	cmd.Execute()
-	fmt.Println("Happy B-day, GoLand")
 }
