f0bdf7be6c20f058bb4ec371cf96a122d2c36a21
D Ther <dtherhtun.cw@gmail.com>
2023-01-10 21:31:32 +0700

    mask

---
+++ nepgo/mask/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		log.Fatalln("Usage: %s dotted-ip-addr ones bits\n", os.Args[0])
+	}
+	dotAddr := os.Args[1]
+	ones, _ := strconv.Atoi(os.Args[2])
+	bits, _ := strconv.Atoi(os.Args[3])
+	addr := net.ParseIP(dotAddr)
+	if addr == nil {
+		log.Fatalln("Invalid address")
+	}
+	mask := net.CIDRMask(ones, bits)
+	computedOnes, computedBits := mask.Size()
+	network := addr.Mask(mask)
+	fmt.Println("Address is ", addr.String(), "\nMask length is", computedBits, "\nLeading ones count is ", computedOnes, "\nMask is (hex)", mask.String(), "\nNetwork is ", network.String())
+}
