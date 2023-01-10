42f5022ec11ad8f7c1c1e71a260e17800f65f54a
D Ther <dtherhtun.cw@gmail.com>
2023-01-10 21:31:32 +0700

    mask

---
+++ nepgo/ipv4mask/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage: %s dotted-ip-addr\n", os.Args[0])
+	}
+	dotAddr := os.Args[1]
+	addr := net.ParseIP(dotAddr)
+	if addr == nil {
+		log.Fatalln("nil Invalid address")
+	}
+	mask := addr.DefaultMask()
+	network := addr.Mask(mask)
+	ones, bits := mask.Size()
+	fmt.Println("Address is ", addr.String(),
+		"\nDefault mask length is ", bits,
+		"\nLeading ones count is ", ones,
+		"\nMask is (hex) ", mask.String(),
+		"\nNetwork is ", network.String())
+	derivedMask := net.IPv4Mask(255, 255, 0, 0) // working on mask
+	fmt.Printf("Network using %s: %s\n", derivedMask, addr.Mask(derivedMask))
+}
