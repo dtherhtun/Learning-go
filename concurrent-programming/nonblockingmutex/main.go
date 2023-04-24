194fdb720a9f1a9f5a48ababa8f0e7a28435d98d
D Ther <dtherhtun.cw@gmail.com>
2023-04-24 09:49:26 +0700

    nonblocking mutex

--- concurrent-programming/nonblockingmutex/main.go
+++ concurrent-programming/nonblockingmutex/main.go
@@ -1,1 +1,48 @@
-package nonblockingmutex
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+	mutex := sync.Mutex{}
+	var frequency = make([]int, 26)
+	for i := 1000; i <= 1200; i++ {
+		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+		go CountLetters(url, frequency, &mutex)
+	}
+	for i := 0; i < 100; i++ {
+		time.Sleep(100 * time.Millisecond)
+		if mutex.TryLock() {
+			for i, c := range AllLetters {
+				fmt.Printf("%c-%d ", c, frequency[i])
+			}
+			mutex.Unlock()
+		} else {
+			fmt.Println("Mutex already being used")
+		}
+	}
+}
+
+const AllLetters = "abcdefghijklmnopqrstuvwxyz"
+
+func CountLetters(url string, frequency []int, mutex *sync.Mutex) {
+	resp, _ := http.Get(url)
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	mutex.Lock()
+	for _, b := range body {
+		c := strings.ToLower(string(b))
+		cIndex := strings.Index(AllLetters, c)
+		if cIndex >= 0 {
+			frequency[cIndex] += 1
+		}
+	}
+	mutex.Unlock()
+	fmt.Println("Completed:", url, time.Now().Format("15:04:05"))
+}
