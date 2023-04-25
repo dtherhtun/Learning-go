b3423d580a6a3ceb3d4cf93ff4e10a119f940e87
D Ther <dtherhtun.cw@gmail.com>
2023-04-25 12:35:03 +0700

    exercise 4.2

--- concurrent-programming/readtrywritemutex/main.go
+++ concurrent-programming/readtrywritemutex/main.go
@@ -24,3 +24,16 @@ func (rw *ReadWriteMutex) WriteLock() {
 func (rw *ReadWriteMutex) TryWriteLock() bool {
 	return rw.globalLock.TryLock()
 }
+
+func (rw *ReadWriteMutex) ReadUnlock() {
+	rw.readersLock.Lock()
+	rw.readersCounter--
+	if rw.readersCounter == 0 {
+		rw.globalLock.Unlock()
+	}
+	rw.readersLock.Unlock()
+}
+
+func (rw *ReadWriteMutex) WriteUnlock() {
+	rw.globalLock.Unlock()
+}
