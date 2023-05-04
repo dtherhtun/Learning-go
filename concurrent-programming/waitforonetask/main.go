1e8c9853305189097d60be837ba9c1b2112641f3
D Ther <dtherhtun.cw@gmail.com>
2023-05-04 23:14:03 +0700

    waitforonetask

---
+++ concurrent-programming/waitforonetask/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func doWork(cond *sync.Cond) {
+	fmt.Println("Work started")
+	fmt.Println("Work finished")
+	cond.L.Lock()
+	cond.Signal()
+	cond.L.Unlock()
+}
+
+func main() {
+	cond := sync.NewCond(&sync.Mutex{})
+	cond.L.Lock()
+	for i := 0; i < 50000; i++ {
+		go doWork(cond)
+		runtime.Gosched()
+		fmt.Println("Waiting for child goroutine")
+		cond.Wait()
+		fmt.Println("Child goroutine finished")
+	}
+	cond.L.Unlock()
+}
