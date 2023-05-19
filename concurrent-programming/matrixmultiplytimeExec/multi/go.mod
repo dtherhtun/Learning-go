f594a23a319b0962cae2161b9e6cb6ab3887b1bf
D Ther <dtherhtun.cw@gmail.com>
2023-05-19 13:16:41 +0700

    single took -> Complete in 12.621737479s | multi took -> Complete in 27.498µs

---
+++ concurrent-programming/matrixmultiplytimeExec/multi/go.mod
@@ -0,0 +1,5 @@
+module github.com/dtherhtun/Learning-go/concurrent-programming/matrixmultiplytimeExec/multi
+
+go 1.20
+
+require github.com/dtherhtun/Learning-go/concurrent-programming/barrier v0.0.0-20230519040653-2f2e99df7f7d
---
+++ concurrent-programming/matrixmultiplytimeExec/multi/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/dtherhtun/Learning-go/concurrent-programming/barrier"
+)
+
+const matrixSize = 3
+
+func generateRandMatrix(matrix *[matrixSize][matrixSize]int) {
+	for row := 0; row < matrixSize; row++ {
+		for col := 0; col < matrixSize; col++ {
+			matrix[row][col] = rand.Intn(10) - 5
+		}
+	}
+}
+
+func rowMultiply(matrixA, matrixB, result *[matrixSize][matrixSize]int, row int, barrier *barrier.Barrier) {
+	for {
+		barrier.Wait()
+		for col := 0; col < matrixSize; col++ {
+			sum := 0
+			for i := 0; i < matrixSize; i++ {
+				sum += matrixA[row][i] * matrixB[i][col]
+			}
+			result[row][col] = sum
+		}
+		barrier.Wait()
+	}
+}
+
+func main() {
+	var matrixA, matrixB, result [matrixSize][matrixSize]int
+	br := barrier.NewBarrier(matrixSize + 1)
+	for row := 0; row < matrixSize; row++ {
+		go rowMultiply(&matrixA, &matrixB, &result, row, br)
+	}
+	start := time.Now()
+	for i := 0; i < 4; i++ {
+		generateRandMatrix(&matrixA)
+		generateRandMatrix(&matrixB)
+		br.Wait()
+		br.Wait()
+	}
+	fmt.Println("Complete in", time.Since(start))
+}
