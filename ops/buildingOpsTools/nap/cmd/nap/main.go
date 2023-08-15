40307a54b1b5c9c6168bb59d4c6307e17596e086
D Ther <dtherhtun.cw@gmail.com>
2023-08-15 22:16:55 +0700

    feat: implement cli

--- ops/buildingOpsTools/nap/cmd/nap/main.go
+++ ops/buildingOpsTools/nap/cmd/nap/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/dtherhtun/Learning-go/ops/buildingOpsTools/nap"
+)
+
+var api = nap.NewAPI("https://httpbin.org")
+
+func main() {
+	list := flag.Bool("list", false, "Get list of all API resources")
+	flag.Parse()
+
+	if *list {
+		fmt.Println("Available Resources: ")
+		for _, name := range api.ResourceNames() {
+			fmt.Println(name)
+		}
+		return
+	}
+
+	resource := os.Args[1]
+	if err := api.Call(resource, nil); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func init() {
+	router := nap.NewRouter()
+	router.RegisterFunc(200, func(resp *http.Response, _ interface{}) error {
+		defer resp.Body.Close()
+		content, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(content))
+		return nil
+	})
+	api.AddResource("get", nap.NewResource("/get", "GET", router))
 }
