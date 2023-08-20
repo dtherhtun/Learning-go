2606dff633808f6bf8c48976de2800347008853e
D Ther <dtherhtun.cw@gmail.com>
2023-08-20 18:02:04 +0700

    feat: (WIP) Clone/Checkout

--- ops/buildingOpsTools/pork/api.go
+++ ops/buildingOpsTools/pork/api.go
@@ -1,7 +1,18 @@
 package pork
 
 import (
-	"github.com/dtherhtun/Learning-go/blob/main/ops/buildingOpsTools/nap"
+	"github.com/dtherhtun/Learning-go/ops/buildingOpsTools/nap"
+	"github.com/spf13/viper"
 )
 
 var api *nap.API
+
+func GitHubAPI() *nap.API {
+	if api == nil {
+		api = nap.NewAPI("https://api.github.com")
+		token := viper.GetString("token")
+		api.SetAuth(nap.NewAuthToken(token))
+		// api.AddResource()
+	}
+	return api
+}
